8d86e3fbe99331af75275086707f2f9e88f94ae7
James Clare <bluecombats@users.noreply.github.com>
2022-01-21 11:19:26 +0000

    Update weakGrowHack.js

--- weakGrowHack.js
+++ weakGrowHack.js
@@ -1,16 +1,16 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	var server = ns.args[0];
-	var threads = ns.args[1];
+	var maxThreads = ns.args[1];
 	var countWeak;
 	while(ns.getServerMoneyAvailable(server)>100){
 		countWeak=0;
 		while(ns.getServerSecurityLevel(server)>ns.getServerMinSecurityLevel(server) && countWeak<10){
-			await ns.weaken(server,{ threads: threads });
+			await ns.weaken(server,{ threads: maxThreads });
 			countWeak+=1;
 		}
-		await ns.grow(server,{ threads: threads });
-		await ns.grow(server,{ threads: threads });
-		await ns.hack(server,{ threads: threads });
+		await ns.grow(server,{ threads: maxThreads });
+		await ns.grow(server,{ threads: maxThreads });
+		await ns.hack(server,{ threads: maxThreads });
 	}
 }
