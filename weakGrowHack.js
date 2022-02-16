73fb65c1ff2fe72e433955590aa69961b892c8f3
James Clare <bluecombats@users.noreply.github.com>
2022-02-16 12:09:49 +0000

    update weakGrowHack.js
    added in weakAnalyze to reduce the number of times weak command gets used

--- weakGrowHack.js
+++ weakGrowHack.js
@@ -1,31 +1,34 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	var server = ns.args[0];
+	var MaxThreads = ns.args[1];
 	var servers = ns.scan();
 	servers.push(server);
-	var maxThreads = ns.args[1];
-	var countGrow,i;
-	var maxMoney =102;
-	while(maxMoney>100){
-		maxMoney = 0;
-		for(i=0; i<servers.length; i++){
+	var maxMoney=102;
+	while (maxMoney>100){
+		maxMoney=0;
+		for(var i=0; i<servers.length; i++){
 			server = servers[i];
-			countGrow=0;
-			if(ns.getServerMoneyAvailable(server)>maxMoney){
-				maxMoney = ns.getServerMoneyAvailable(server);
-			}
-			while(ns.getServerMoneyAvailable(server)>100){
-				while(ns.getServerSecurityLevel(server)>ns.getServerMinSecurityLevel(server)){
-					await ns.weaken(server,{ threads: maxThreads });
+			if(ns.hasRootAccess(server)){
+				if(ns.getServerMaxMoney(server)>100){
+					maxMoney = ns.getServerMaxMoney(server);
 				}
-				while(ns.getServerMaxMoney(server)<ns.getServerMoneyAvailable(server) && countGrow<10){
-					await ns.grow(server,{ threads: maxThreads });
-					while(ns.getServerSecurityLevel(server)>ns.getServerMinSecurityLevel(server)){
-						await ns.weaken(server,{ threads: maxThreads });
+				//weaken
+				while((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(MaxThreads)) 
+				>= ns.getServerMinSecurityLevel(server)){
+					await ns.weaken(server,{ threads: MaxThreads });
+				}
+				//grow, weaken
+				while(ns.getServerMoneyAvailable(server)<ns.getServerMaxMoney(server)){
+					await ns.grow(server,{ threads: MaxThreads });
+					//weaken
+					while((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(MaxThreads)) 
+					>= ns.getServerMinSecurityLevel(server)){
+						await ns.weaken(server,{ threads: MaxThreads });
 					}
-					countGrow+=1;
 				}
-				await ns.hack(server,{ threads: maxThreads });
+				//hack
+				await ns.hack(server,{ threads: MaxThreads });
 			}
 		}
 	}
