c9a9c1eeb5eab2a24f47a6a263988c490e1f56fa
James Clare <bluecombats@users.noreply.github.com>
2022-02-16 11:36:19 +0000

    updated serverHack.js
    added in fx about scriptram to maximise amount of threads weakGrowHack.js can use

--- serverHack.js
+++ serverHack.js
@@ -5,18 +5,21 @@ function delay(milliseconds){
     });
 }
 export async function main(ns) {
-	var count = 0,i;
+	var count=0,i,j;
 	var scan = ns.scan();
-	var server,portsCount,threads,ramLeft;
-	server = ns.getServer();
-	scan.push(server["hostname"]);
-	while(count<scan.length){
-		count = 0;
+	var server, portsCount, portsReq, threads;
+	//server = ns.getHostname()
+	server = ns.getServer()["hostname"];	
+	scan.push(server);
+	ns.tprint(scan);
+	while(scan.length != count){
+		//ns.tprint("scan length: ",scan.length);
 		await delay(1000);
+		count=0;
 		for(i=0; i<scan.length; i++){
-			//ns.tprint(scan[i]);
+			server = scan[i];
+			//ns.tprint(server);
 			threads=1;
-			server = ns.getServer(scan[i]);
 			/*{"cpuCores":1
 			,"ftpPortOpen":false
 			,"hasAdminRights":false
@@ -41,47 +44,66 @@ export async function main(ns) {
 			,"openPortCount":0
 			,"requiredHackingSkill":1
 			,"serverGrowth":5}*/
-			if(ns.hasRootAccess(scan[i])){
+			//ns.tprint("hack lvl: ",ns.getHackingLevel()
+			//,"server level: ",ns.getServer(server)["requiredHackingSkill"])
+			if(!ns.fileExists("weakGrowHack.js",scan[i])){
+				await ns.scp("weakGrowHack.js",scan[i]);
+			}
+			//if(ns.hasRootAccess(server)){
+			if(ns.getServer(server)["hasAdminRights"]){
+			//ns.hasRootAccess(scan[i])){
 				count+=1;
 				if(!ns.fileExists("serverHack.js",scan[i])){
 					await ns.scp("serverHack.js",scan[i]);
 					await ns.exec("serverHack.js",scan[i], 1);
 				}
-				ramLeft = server["maxRam"] - server["ramUsed"];
-				if(ramLeft>=32)		{threads=12}
-				else if(ramLeft>=16)	{threads=6}
-				else if(ramLeft>=8)	{threads=3}
-				else if(ramLeft>=4)	{threads=1}
-				if(!ns.fileExists("weakGrowHack.js",scan[i])){
-					await ns.scp("weakGrowHack.js",scan[i]);
+				//then copy over hack script
+				var ramLeft = ns.getServer(server)["maxRam"] - ns.getServer(server)["ramUsed"];
+				//var ramLeft = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+				//get max threads you can use
+				var scriptRam = ns.getScriptRam("weakGrowHack.js",server);
+				for(j=Math.floor(ramLeft); j>0; j--){
+					if(scriptRam*j<=ramLeft){
+						threads=j;
+						break;
+					}
 				}
-				if(!ns.scriptRunning("weakGrowHack.js",scan[i])){
-					await ns.exec("weakGrowHack.js",scan[i], threads, scan[i], threads);
-				}				
+				if(!ns.scriptRunning("weakGrowHack.js", scan[i])){
+					await ns.exec("weakGrowHack.js", scan[i], threads, scan[i], threads);
+				}
 			}
-			else if(ns.getHackingLevel()>=ns.getServerRequiredHackingLevel(scan[i])){
+			//else if(ns.getHackingLevel()>= ns.getServerRequiredHackingLevel(server)){
+			else if(ns.getHackingLevel()>=ns.getServer(server)["requiredHackingSkill"]){
 				//how many ports are open
-				if(!server["ftpPortOpen"] && ns.fileExists("FTPCrack.exe","home")){
+				if(!ns.getServer(server)["ftpPortOpen"] && ns.fileExists("FTPCrack.exe","home")){
 					ns.ftpcrack(scan[i]);
 				}
-				if(!server["httpPortOpen"] && ns.fileExists("HTTPWorm.exe","home")){
+				if(!ns.getServer(server)["httpPortOpen"] && ns.fileExists("HTTPWorm.exe","home")){
 					ns.httpworm(scan[i]);
 				}
-				if(!server["smtpPortOpen"] && ns.fileExists("relaySMTP.exe","home")){
+				if(!ns.getServer(server)["smtpPortOpen"] && ns.fileExists("relaySMTP.exe","home")){
 					ns.relaysmtp(scan[i]);
 				}
-				if(!server["sshPortOpen"] && ns.fileExists("BruteSSH.exe","home")){
+				if(!ns.getServer(server)["sshPortOpen"] && ns.fileExists("BruteSSH.exe","home")){
 					ns.brutessh(scan[i]);
 				}
-				if(!server["sqlPortOpen"] && ns.fileExists("SQLInject.exe","home")){
+				if(!ns.getServer(server)["sqlPortOpen"] && ns.fileExists("SQLInject.exe","home")){
 					ns.sqlinject(scan[i]);
 				}
 				//can it be nuked
-				portsCount = ns.getServer(scan[i])["openPortCount"];
-				if(portsCount >= ns.getServerNumPortsRequired(scan[i])){
+				portsCount = ns.getServer(server)["openPortCount"];
+				//if(ns.getServer(server)["numOpenPortsRequired"]="null"){portsReq=0}
+				//else(
+					portsReq = ns.getServer(server)["numOpenPortsRequired"]
+				//)
+				//ns.tprint("open ports: ",portsCount);
+				//ns.tprint("req ports: ",portsReq);
+				//if(portsCount >= ns.getServerNumPortsRequired(scan[i])){
+				if(portsCount >= portsReq){
 					ns.nuke(scan[i]);
 				}
 			}
 		}
+		//ns.tprint("servers open:",count);
 	}
 }
