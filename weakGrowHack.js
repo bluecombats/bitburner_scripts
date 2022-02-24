8c39430f799b185c1d9223370b40c1d9cf7d78a1
James Clare <bluecombats@users.noreply.github.com>
2022-02-24 15:42:25 +0000

    Update weakGrowHack.js
    added in a flexible weak, so it doesn't always use max threads but the most that is needed to get the job done. And said to ignore hacking home server

--- weakGrowHack.js
+++ weakGrowHack.js
@@ -5,27 +5,43 @@ export async function main(ns) {
 	var servers = ns.scan();
 	servers.push(server);
 	var maxMoney=102;
+	var growCount;
 	while (maxMoney>100){
 		maxMoney=0;
 		for(var i=0; i<servers.length; i++){
 			server = servers[i];
+			if(server = "home"){continue}
+			growCount=0;
 			if(ns.hasRootAccess(server)){
 				if(ns.getServerMaxMoney(server)>100){
 					maxMoney = ns.getServerMaxMoney(server);
 				}
 				//weaken
-				while((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(MaxThreads)) 
+				while((ns.getServerSecurityLevel(server)) 
 				>= ns.getServerMinSecurityLevel(server)){
-					await ns.weaken(server,{ threads: MaxThreads });
+					for(var j=1; j<MaxThreads;j++){
+						if((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(j)) 
+						>= ns.getServerMinSecurityLevel(server)){
+							break;
+						}
+					}
+					await ns.weaken(server,{ threads: j });
 				}
 				//grow, weaken
-				while(ns.getServerMoneyAvailable(server)<ns.getServerMaxMoney(server)){
+				while(ns.getServerMoneyAvailable(server)<ns.getServerMaxMoney(server) && growCount<100){
 					await ns.grow(server,{ threads: MaxThreads });
 					//weaken
-					while((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(MaxThreads)) 
+					while((ns.getServerSecurityLevel(server)) 
 					>= ns.getServerMinSecurityLevel(server)){
-						await ns.weaken(server,{ threads: MaxThreads });
+						for(var j=1; j<MaxThreads;j++){
+							if((ns.getServerSecurityLevel(server) - ns.weakenAnalyze(j)) 
+							>= ns.getServerMinSecurityLevel(server)){
+								break;
+							}
+						}
+						await ns.weaken(server,{ threads: j });
 					}
+					growCount+=1;
 				}
 				//hack
 				await ns.hack(server,{ threads: MaxThreads });
