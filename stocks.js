33aef1b806612b6e7cf44bc648811f2dd461cf05
James Clare <bluecombats@users.noreply.github.com>
2022-03-03 12:44:56 +0000

    Update stocks.js
    made sure I have money before I buy stock and maxshares is postivie

--- stocks.js
+++ stocks.js
@@ -6,22 +6,31 @@ function delay(milliseconds){
 }
 export async function main(ns) {
 	var symbols = ns.stock.getSymbols();
-    var maxShares;
-    while(ns.getPlayer()["money"]>200000){
-        await delay(6000);
-        for(var i=0; i<symbols.length; i++){
-            var smb = symbols[i];
+    var maxShares,i,smb,smbStr,minMoney=200000;
+    while(ns.getPlayer()["money"]>minMoney){
+        await delay(1000);
+        for(i=0; i<symbols.length; i++){
+            smb = symbols[i];
+		    smbStr = smb + "   ";
+		    smbStr = smbStr.substr(0,5);
             if(ns.stock.getVolatility(smb)<1){
                 //if I have no long stocks, then buy
+                //ns.tprint(smbStr," long pos: ",ns.stock.getPosition(smb)[0]);
                 if(ns.stock.getPosition(smb)[0]==0){
-                    if(ns.stock.getForecast(smb)>0.69){
-                        maxShares = ns.getPlayer()["money"]/ns.stock.getPrice(smb);
+                    //ns.tprint(smbStr," for: ",ns.stock.getForecast(smb));
+                    if(ns.stock.getForecast(smb)>0.60 && ns.getPlayer()["money"]>(minMoney+100000)){
+                        maxShares = (ns.getPlayer()["money"]-minMoney)/ns.stock.getAskPrice(smb);
+                        if(Math.floor(maxShares)>ns.stock.getMaxShares(smb)){
+                            maxShares = ns.stock.getMaxShares(smb);
+                        }
+                        ns.print(smbStr," MShares: ",maxShares);
                         ns.stock.buy(smb,Math.floor(maxShares));
                     }
                 }
                 //else sell
                 else{
-                    if(ns.stock.getForecast(smb)<0.31){
+                    ns.print(smbStr," Lpos: ",ns.stock.getPosition(smb)[0]," for: ",ns.stock.getForecast(smb));
+                    if(ns.stock.getForecast(smb)<0.5){
                         maxShares = ns.stock.getPosition(smb)[0];
                         ns.stock.sell(smb, maxShares);
                     }
