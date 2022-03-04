53a6ed28617413810670f801dbefc2b5d69bc6f5
James Clare <bluecombats@users.noreply.github.com>
2022-03-04 17:56:00 +0000

    Update stocks.js
    made sure it doesn't stop early

--- stocks.js
+++ stocks.js
@@ -8,34 +8,39 @@ export async function main(ns) {
 	var symbols = ns.stock.getSymbols();
     var maxShares,i,smb,smbStr,minMoney=200000;
     while(ns.getPlayer()["money"]>minMoney){
+        ns.print("money @ start: ",ns.getPlayer()["money"]);
         await delay(1000);
         for(i=0; i<symbols.length; i++){
             smb = symbols[i];
 		    smbStr = smb + "   ";
 		    smbStr = smbStr.substr(0,5);
-            if(ns.stock.getVolatility(smb)<1){
-                //if I have no long stocks, then buy
-                //ns.tprint(smbStr," long pos: ",ns.stock.getPosition(smb)[0]);
-                if(ns.stock.getPosition(smb)[0]==0){
-                    //ns.tprint(smbStr," for: ",ns.stock.getForecast(smb));
+            //ns.tprint(smbStr," long pos: ",ns.stock.getPosition(smb)[0]);
+            //if I have no long stocks, then buy       
+            if(ns.stock.getPosition(smb)[0] == 0){
+                if(ns.stock.getVolatility(smb)<1){
+                    //ns.print(smbStr," for: ",ns.stock.getForecast(smb));
                     if(ns.stock.getForecast(smb)>0.60 && ns.getPlayer()["money"]>(minMoney+100000)){
-                        maxShares = (ns.getPlayer()["money"]-minMoney)/ns.stock.getAskPrice(smb);
+                        maxShares = (ns.getPlayer()["money"]-minMoney+100000)/ns.stock.getAskPrice(smb);
                         if(Math.floor(maxShares)>ns.stock.getMaxShares(smb)){
                             maxShares = ns.stock.getMaxShares(smb);
                         }
-                        ns.print(smbStr," MShares: ",maxShares);
-                        ns.stock.buy(smb,Math.floor(maxShares));
-                    }
-                }
-                //else sell
-                else{
-                    ns.print(smbStr," Lpos: ",ns.stock.getPosition(smb)[0]," for: ",ns.stock.getForecast(smb));
-                    if(ns.stock.getForecast(smb)<0.5){
-                        maxShares = ns.stock.getPosition(smb)[0];
-                        ns.stock.sell(smb, maxShares);
+                        if((ns.getPlayer()["money"]-(maxShares *ns.stock.getAskPrice(smb)))>200000){
+                            ns.print(smbStr," MShares: ",Math.floor(maxShares));
+                            ns.stock.buy(smb,Math.floor(maxShares));
+                        }
                     }
                 }
             }
+            //else sell
+            else{
+                ns.print(smbStr," Lpos: ",ns.stock.getPosition(smb)[0]," for: ",ns.stock.getForecast(smb));
+                if(ns.stock.getForecast(smb)<0.5){
+                    maxShares = ns.stock.getPosition(smb)[0];
+                    ns.print("sell ",smb,)
+                    ns.stock.sell(smb, maxShares);
+                }
+            }
         }
+        ns.print("money @ end: ",ns.getPlayer()["money"]);
     }
 }
