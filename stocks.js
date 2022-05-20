6209fa4b2849dbf2f7eecba30db91fd74af5a868
James Clare <bluecombats@users.noreply.github.com>
2022-05-20 19:45:48 +0100

    Update Stocks.js
    added a better way to display money when tailing script

--- stocks.js
+++ stocks.js
@@ -4,11 +4,31 @@ function delay(milliseconds){
         setTimeout(resolve, milliseconds);
     });
 }
+function Moneyformat(money){
+    var moneySplit;
+    if(Math.abs(money/Math.pow(10,12)) >1){
+        moneySplit = money/Math.pow(10,12);
+        return "$"+String(moneySplit).substr(0,7)+"tr";
+    }
+    else if(Math.abs(money/Math.pow(10,9)) >1){
+        moneySplit = money/Math.pow(10,9);
+        return "$"+String(moneySplit).substr(0,7)+"bn";
+    }else if(Math.abs(money/Math.pow(10,6)) >1){
+        moneySplit = money/Math.pow(10,6);
+        return "$"+String(moneySplit).substr(0,7)+"m";
+    }else if(Math.abs(money/Math.pow(10,3)) >1){
+        moneySplit = money/Math.pow(10,3);
+        return "$"+String(moneySplit).substr(0,7)+"k";
+    }
+    else{
+        return "$"+String(money);
+    }
+}
 export async function main(ns) {
 	var symbols = ns.stock.getSymbols();
     var maxShares,i,smb,smbStr,minMoney=200000;
-    while(ns.getPlayer()["money"]>minMoney){
-        ns.print("money @ start: ",ns.getPlayer()["money"]);
+    while(ns.getPlayer()["money"]>0){
+        ns.print("money @ start: ",Moneyformat(ns.getPlayer()["money"]));
         await delay(3000);
         for(i=0; i<symbols.length; i++){
             smb = symbols[i];
@@ -19,13 +39,14 @@ export async function main(ns) {
             if(ns.stock.getPosition(smb)[0] == 0){
                 //ask price is buy price, and bid price is sell price
                 if(ns.stock.getVolatility(smb)<1){
-                    //ns.print(smbStr," for: ",ns.stock.getForecast(smb));
+                    //ns.print(smbStr," for: ",String(ns.stock.getForecast(smb)).substr(0,4));
                     if(ns.stock.getForecast(smb)>0.60 && ns.getPlayer()["money"]>(minMoney+100000)){
                         maxShares = (ns.getPlayer()["money"]-minMoney+100000)/ns.stock.getAskPrice(smb);
                         maxShares = Math.floor(maxShares);
                         if(maxShares>ns.stock.getMaxShares(smb)){
                             maxShares = ns.stock.getMaxShares(smb);
                         }
+                        ns.print(smbStr," MaxShares: ",ns.stock.getMaxShares(smb)," calcMaxShares: ",maxShares);
                         for(var j=maxShares;maxShares>0;j--){
                             //ns.print(smb," ",j);
                             if((ns.getPlayer()["money"]-(j *ns.stock.getAskPrice(smb)))>(minMoney+100000)){
@@ -44,7 +65,7 @@ export async function main(ns) {
             else{
                 maxShares = ns.stock.getPosition(smb)[0];
                 ns.print(smbStr," for: ",String(ns.stock.getForecast(smb)).substr(0,4)," Lpos: ",ns.stock.getPosition(smb)[0]
-                    , " Gain: ", ns.stock.getSaleGain(smb,maxShares,"Long"));
+                    , " Gain: ", Moneyformat(ns.stock.getSaleGain(smb,maxShares,"Long")));
                 if(ns.stock.getForecast(smb)<0.5 && ns.stock.getSaleGain(smb,maxShares,"Long") > 0){
                     //maxShares*ns.stock.getBidPrice(smb)
                     ns.print("sell ",smb," SaleGain: ",ns.stock.getSaleGain(smb,maxShares,"Long"))
@@ -52,6 +73,6 @@ export async function main(ns) {
                 }
             }
         }
-        ns.print("money @ end: ",ns.getPlayer()["money"]);
+        ns.print("money @ end: ",Moneyformat(ns.getPlayer()["money"]));
     }
 }
