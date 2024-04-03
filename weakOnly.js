370e49675daa54127ecdc1cff2bbe49b2d42ce9b
James Clare <bluecombats@users.noreply.github.com>
2024-04-03 11:06:24 +0100

    Create weakOnly.js

---
+++ weakOnly.js
@@ -0,0 +1,35 @@
+/** @param {NS} ns */
+function delay(milliseconds){
+	return new Promise(resolve => {
+			setTimeout(resolve, milliseconds);
+	});
+}
+function max_money(ns,servers){
+	var server,i,max_money=0;
+	for(i=0;i<servers.length;i++){
+		if(servers[i]=="home"){
+			continue;
+		}
+		if(ns.getServerMaxMoney(servers[i])>max_money && ns.hasRootAccess(servers[i])){
+			server = servers[i];
+			max_money = ns.getServerMaxMoney(servers[i]);
+		}
+	}
+	return server;
+}
+export async function main(ns) {
+	var server = ns.args[0];
+	var threads = ns.args[1];
+	var servers = ns.scan();
+	var i;
+	servers.push(server);
+	while(server){
+		server = max_money(ns,servers);
+		ns.print("weak server: ",server);
+		// if(!server)
+		if(ns.hasRootAccess(server)){
+			await ns.weaken(server,{ threads: threads });
+		}
+		await delay(1000*1)
+	}
+}
